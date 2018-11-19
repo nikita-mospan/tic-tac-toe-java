4fc2af64c10e52869ee115d66c194197e2df8335
Nikita Mospan <mospan.nn@phystech.edu>
2018-11-19 16:17:38 +0300

    Do not create copies of Board objects

--- src/main/java/mospan/tic_tac_toe/Board.java
+++ src/main/java/mospan/tic_tac_toe/Board.java
@@ -14,23 +14,6 @@ public class Board {
     private CellValue opponentPlayerValue;
     private static final int BOARD_LENGTH = 3;
 
-    private Player getOpponentPlayer() {
-        return opponentPlayer;
-    }
-
-    private Player getCurrentPlayer() {
-        return currentPlayer;
-    }
-
-    private CellValue getCurrentPlayerValue() {
-        return currentPlayerValue;
-    }
-
-    private CellValue getOpponentPlayerValue() {
-        return opponentPlayerValue;
-    }
-
-
     Board(Player currentPlayer) {
         this.board = new CellValue[][]{
                 {NAUGHT, EMPTY, CROSS},
@@ -41,17 +24,6 @@ public class Board {
         this.currentPlayerValue = CROSS;
         this.opponentPlayer = currentPlayer == HUMAN ? COMPUTER : HUMAN;
         this.opponentPlayerValue = NAUGHT;
-    }
-
-    private Board(Board board) {
-        this.board = new CellValue[BOARD_LENGTH][BOARD_LENGTH];
-        for (int rowIdx = 0; rowIdx < BOARD_LENGTH; rowIdx++) {
-            System.arraycopy(board.getBoard()[rowIdx], 0, this.board[rowIdx], 0, BOARD_LENGTH);
-        }
-        this.currentPlayer = board.getCurrentPlayer();
-        this.currentPlayerValue = board.getCurrentPlayerValue();
-        this.opponentPlayer = board.getOpponentPlayer();
-        this.opponentPlayerValue = board.getOpponentPlayerValue();
     }
 
     private void switchPlayers() {
@@ -79,15 +51,16 @@ public class Board {
         for (int rowIdx = 0; rowIdx < BOARD_LENGTH; rowIdx++) {
             for (int columnIdx = 0; columnIdx < BOARD_LENGTH; columnIdx++) {
                 if (board[rowIdx][columnIdx] == EMPTY) {
-                    Board boardWithNewMove = new Board(this);
-                    boardWithNewMove.getBoard()[rowIdx][columnIdx] = currentPlayerValue;
-                    boardWithNewMove.switchPlayers();
-                    nextOptimalMove = boardWithNewMove.makeNextMove();
+                    board[rowIdx][columnIdx] = currentPlayerValue;
+                    switchPlayers();
+                    nextOptimalMove = makeNextMove();
                     scoreForNextMove = -nextOptimalMove.getScore();
                     if (scoreForNextMove > score) {
                         score = scoreForNextMove;
                         optimalMove = new OptimalMove(rowIdx, columnIdx, score);
                     }
+                    board[rowIdx][columnIdx] = EMPTY;
+                    switchPlayers();
                 }
             }
         }
@@ -134,7 +107,4 @@ public class Board {
         return resultBuilder.toString();
     }
 
-    private CellValue[][] getBoard() {
-        return board;
-    }
 }
